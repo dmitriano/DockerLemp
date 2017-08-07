a95cad322d00ba61fc14afc39beb41adb3d4c8a1
dmitriano <winappdev@gmail.com>
2017-08-07 18:27:41 +0000

    added ClamAV

---
+++ u1204php53/sh/run.sh
@@ -0,0 +1,2 @@
+sudo docker run -d --restart=always --name u12php53 -v /home/beauty/www:/home/beauty/www -v /home/omegauto/www:/home/omegauto/www -v /home/master/www:/home/master/www php53s
+
---
+++ u1604clam/sh/run.sh
@@ -0,0 +1,3 @@
+sudo docker run -it --rm --name clam -v /home/beauty/www:/home/beauty/www/ u16clam
+#run 'clamscan -r -i .' in the container
+
