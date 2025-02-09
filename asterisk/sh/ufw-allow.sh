228cf5c400648c0fea6cee5bf484bd586e5d3a7d
dmitriano <windappdev@gmail.com>
2025-02-10 02:54:12 +0300

    pjsip

--- asterisk/sh/ufw-allow.sh
+++ asterisk/sh/ufw-allow.sh
@@ -1,3 +1,4 @@
 sudo ufw allow 5060/udp
+sudo ufw allow 5060/tcp
 sudo ufw allow 5061/tcp
 sudo ufw allow 10000:10099/udp
--- asterisk/sh/ufw-delete.sh
+++ asterisk/sh/ufw-delete.sh
@@ -1,3 +1,4 @@
 sudo ufw delete allow 5060/udp
 sudo ufw delete allow 5061/tcp
+sudo ufw delete allow 5061/tcp
 sudo ufw delete allow 10000:10099/udp
